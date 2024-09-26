3ff4368b5830cd66f1aae3037877b0454b27434c
Henrique Gonçalves <henrique.gonc.gamer@gmail.com>
2024-09-25 21:54:46 -0300

    Fiz o Usuario

---
+++ Models/Arma.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Net.Http.Headers;
+
+namespace GamingAPI.Models
+{
+    public class Arma
+    {
+        public int Id {get; set;}
+        public string Nome {get; set;} = "";
+        public int Dano {get; set;}
+    }
+}
---
+++ Models/Personagem.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GamingAPI.Models;
+using RpgApi.Models.Enuns;
+
+namespace RpgApi.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        public ClasseEnum Classe { get; set; }
+
+        public byte[]? FotoPersonagem { get; set; }
+        public int? UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+
+        internal static List<Personagem> FindAll(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
