f386b5a82c4967c93ec178c6c4c61b4f16a75514
HenriqueGoncalves22 <henrique.gonc.gamer@gmail.com>
2024-10-02 19:47:18 -0300

    Criei o script 5

--- Migrations/20241002223802_MigracaoMuitosParaMuitos.Designer.cs
+++ Migrations/20241002223802_MigracaoMuitosParaMuitos.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RpgApi.Data;
 
@@ -11,9 +12,11 @@ using RpgApi.Data;
 namespace RpgApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241002223802_MigracaoMuitosParaMuitos")]
+    partial class MigracaoMuitosParaMuitos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -442,7 +445,7 @@ namespace RpgApi.Migrations
                         .IsRequired();
 
                     b.HasOne("RpgApi.Models.Personagem", "Personagem")
-                        .WithMany()
+                        .WithMany("PersonagemHabilidades")
                         .HasForeignKey("PersonagemId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
