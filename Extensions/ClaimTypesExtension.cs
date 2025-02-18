369f06aedc6897f883e90f59aeed860392a720b7
Henrique Gonçalves <henrique.gonc.gamer@gmail.com>
2025-02-18 19:53:01 -0300

    Fiz a Extension

---
+++ Extensions/ClaimTypesExtension.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace RpgApi.Extensions
+{
+    public static class ClaimTypesExtension
+    {
+        public static int UsuarioId(this ClaimsPrincipal user)
+        {
+            try
+            {
+                var usuarioId = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+                return int.Parse(usuarioId);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static string UsuarioPerfil(this ClaimsPrincipal user)
+        {
+            try
+            {
+               var UsuarioPerfil = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value ?? string.Empty;
+               return UsuarioPerfil;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        
+    }
+}
